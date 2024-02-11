80ed6d64fd184bb1767e8fecf3470eff42a20b06
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-02-11 18:19:06 -0400

    Agregar archivos de proyecto.

---
+++ contraseña intentos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("3 intentos");
+Console.WriteLine();
+
+        string contraseña = "YILEINI123"; 
+        int intentosMaximos = 3; 
+        int intentos = 0; 
+
+        while (intentos < intentosMaximos)
+        {
+            Console.WriteLine("Ingrese la contraseña:");
+            string contraseñaIngresada = Console.ReadLine();
+
+            if (contraseñaIngresada == contraseña)
+            {
+                Console.WriteLine("¡Contraseña correcta!");
+                return; 
+            }
+            else
+            {
+                intentos++;
+                Console.WriteLine("Contraseña incorrecta. Intentos restantes: " + (intentosMaximos - intentos));
+            }
+        }
+
+        // Si se llega a este punto, significa que se agotaron los intentos
+        Console.WriteLine("Se ha agotado el número de intentos. El programa terminará.");
+    
+
+
